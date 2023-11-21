9113ce7fb0f85ff6414b60c6817c47eee7884934
NakibingeSteven <88508645+NakibingeSteven@users.noreply.github.com>
2023-11-21 13:29:39 +0300

    Printing more data types

--- HelloWorld/main.go
+++ HelloWorld/main.go
@@ -2,6 +2,16 @@ package main
 
 import "fmt"
 
+// constants
+const P1 = 3.14
+
 func main() {
-    fmt.Println("Hello, World Nakibinge !")
+	//variables
+	var names string = "Nakibinge Steven"
+	number := 56 //type inferred
+	//print out statement
+	fmt.Println(names)
+	fmt.Println(number)
+	fmt.Println(P1)
+	fmt.Println("Hello, World Nakibinge !")
 }
